e3ddcf114f04b998fe963e665781a56e12ff79f8
amirmahdi-alavi <alavi_amirmahdi@yahoo.com>
2023-11-07 22:23:56 +0330

    added signup page view and url

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, HttpResponse, redirect
-from django.views.generic import View
+from django.views.generic import View, FormView
 from django.contrib.auth import authenticate
 from django.contrib import messages
+from .forms import CreateUserForm
 
 # Create your views here.
 class Login(View):
@@ -19,5 +22,24 @@ class Login(View):
         return redirect('login')
 
 
-class Signup(View):
-    pass
+class Signup(FormView):
+    form_class = CreateUserForm
+    template_name = 'signup.html'
+    success_url = '/accounts/login/'
+
+    # def form_valid(self, form):
+    #     form.save()
+    #     success_message = messages.success(self.request, 'Success!')
+    #     return super().form_valid(form)
+    
+
+    # def form_valid(self, form):
+    #     user = form.save()
+    #     messages.success(self.request, 'Account created successfully. You can now log in.')
+    #     return self.render_to_response(self.get_context_data(form=form, success=True))
+
+
+    def form_valid(self, form):
+            user = form.save()
+            messages.success(self.request, 'Account created successfully. You can now log in.')
+            return super().form_valid(form)
