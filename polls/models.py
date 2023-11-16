5ca2ed51fd431f6fb7a1a9a839cee4d5b9c68d60
amirmahdi-alavi <alavi_amirmahdi@yahoo.com>
2023-11-17 01:43:47 +0330

    updated polls database model and logic

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
-from django.contrib.contenttypes.models import ContentType
 from accounts.models import User
-from django.utils import timezone
 # Create your models here.
 
 
@@ -37,10 +34,21 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question= models.ForeignKey(Question, models.CASCADE, related_name='choices')
-    is_text = models.BooleanField(default=False)
-    value = models.CharField(max_length=255)
+    # is_text = models.BooleanField(default=False)
+    title = models.CharField(max_length=150, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def is_text(self):
+        if self.question.question_type.question_type == 'text':
+            return True
+        return False
+
+    def save(self, *args, **kwargs):
+        if self.is_text:
+            return None
+        super(Choice, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"for question: {self.question_id}, added choice: {self.value}"
@@ -48,14 +56,28 @@ class Choice(models.Model):
 
 class Answer(models.Model):
     user = models.ForeignKey(User, related_name='answers', on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, related_name='answers', on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, related_name='answers', on_delete=models.CASCADE, blank=True, null=True)
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    text_value = models.TextField(null=True, blank=True)
     answer_time = models.DateTimeField(auto_now_add=True)
 
     @property
     def question_type(self):
         return self.question.question_type
-    
+
+    @property
+    def is_text(self):
+        if self.question.question_type.question_type == 'text':
+            return True
+        return False
+
+    def save(self, *args, **kwargs):
+        if not self.is_text:
+            self.text_value = None
+        else:
+            self.choice = None
+        super(Answer, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"user {self.user.first_name + ' ' + self.user.last_name},\
           answered question {self.question.title} with {self.choice.value} choice."
